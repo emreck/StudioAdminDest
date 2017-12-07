965df02207b395d8d8a82f85cbee66883c737759
mandalbatmaz21 <nakipali1@gmail.com>
2017-12-07 22:58:57 +0300

    İlk test commiti

--- StudioAdminDest/Form1.Designer.cs
+++ StudioAdminDest/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(139, 125);
+            this.button1.Location = new System.Drawing.Point(168, 193);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Yeni Buton";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
