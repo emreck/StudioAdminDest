cf4d247c58bdfd7428ea98e819dd1606989e3ec5
figo_9991 <figo_9991@hotmail.com>
2017-12-09 21:11:56 +0300

    Menü tasarımı gerçekleşti.

--- StudioAdminDest/Form1.Designer.cs
+++ StudioAdminDest/Form1.Designer.cs
@@ -28,71 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnKayit = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.btnSil = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.personelİşlemleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personelKayıtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personelGörüntüleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.müşteriİşlemleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.işOluşturToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yapılacakİşlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.giderlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fiyatlandırmaİşlemleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.baskıİşleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kasaİşlemleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dosyaİşlemleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnKayit
+            // menuStrip1
             // 
-            this.btnKayit.Location = new System.Drawing.Point(173, 222);
-            this.btnKayit.Name = "btnKayit";
-            this.btnKayit.Size = new System.Drawing.Size(75, 23);
-            this.btnKayit.TabIndex = 0;
-            this.btnKayit.Text = "Yeni Buton";
-            this.btnKayit.UseVisualStyleBackColor = true;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.personelİşlemleriToolStripMenuItem,
+            this.müşteriİşlemleriToolStripMenuItem,
+            this.giderlerToolStripMenuItem,
+            this.fiyatlandırmaİşlemleriToolStripMenuItem,
+            this.baskıİşleriToolStripMenuItem,
+            this.kasaİşlemleriToolStripMenuItem,
+            this.dosyaİşlemleriToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(735, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // button2
+            // personelİşlemleriToolStripMenuItem
             // 
-            this.button2.Location = new System.Drawing.Point(173, 193);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "03 50 proje";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.personelİşlemleriToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.personelKayıtToolStripMenuItem,
+            this.personelGörüntüleToolStripMenuItem});
+            this.personelİşlemleriToolStripMenuItem.Name = "personelİşlemleriToolStripMenuItem";
+            this.personelİşlemleriToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
+            this.personelİşlemleriToolStripMenuItem.Text = "Personel işlemleri";
             // 
-            // btnSil
+            // personelKayıtToolStripMenuItem
             // 
-            this.btnSil.Location = new System.Drawing.Point(323, 189);
-            this.btnSil.Name = "btnSil";
-            this.btnSil.Size = new System.Drawing.Size(75, 56);
-            this.btnSil.TabIndex = 2;
-            this.btnSil.Text = "Ali YAZMACI";
-            this.btnSil.UseVisualStyleBackColor = true;
+            this.personelKayıtToolStripMenuItem.Name = "personelKayıtToolStripMenuItem";
+            this.personelKayıtToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.personelKayıtToolStripMenuItem.Text = "Personel Kayıt";
             // 
-            // dataGridView1
+            // personelGörüntüleToolStripMenuItem
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(8, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(390, 150);
-            this.dataGridView1.TabIndex = 3;
+            this.personelGörüntüleToolStripMenuItem.Name = "personelGörüntüleToolStripMenuItem";
+            this.personelGörüntüleToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.personelGörüntüleToolStripMenuItem.Text = "Personel Görüntüle";
+            // 
+            // müşteriİşlemleriToolStripMenuItem
+            // 
+            this.müşteriİşlemleriToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.işOluşturToolStripMenuItem,
+            this.yapılacakİşlerToolStripMenuItem,
+            this.bToolStripMenuItem});
+            this.müşteriİşlemleriToolStripMenuItem.Name = "müşteriİşlemleriToolStripMenuItem";
+            this.müşteriİşlemleriToolStripMenuItem.Size = new System.Drawing.Size(106, 20);
+            this.müşteriİşlemleriToolStripMenuItem.Text = "Müşteri İşlemleri";
+            this.müşteriİşlemleriToolStripMenuItem.Click += new System.EventHandler(this.müşteriİşlemleriToolStripMenuItem_Click);
+            // 
+            // işOluşturToolStripMenuItem
+            // 
+            this.işOluşturToolStripMenuItem.Name = "işOluşturToolStripMenuItem";
+            this.işOluşturToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.işOluşturToolStripMenuItem.Text = "İş oluştur";
+            // 
+            // yapılacakİşlerToolStripMenuItem
+            // 
+            this.yapılacakİşlerToolStripMenuItem.Name = "yapılacakİşlerToolStripMenuItem";
+            this.yapılacakİşlerToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.yapılacakİşlerToolStripMenuItem.Text = "Yapılacak İşler";
+            // 
+            // bToolStripMenuItem
+            // 
+            this.bToolStripMenuItem.Name = "bToolStripMenuItem";
+            this.bToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bToolStripMenuItem.Text = "Biten İşler";
+            // 
+            // giderlerToolStripMenuItem
+            // 
+            this.giderlerToolStripMenuItem.Name = "giderlerToolStripMenuItem";
+            this.giderlerToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.giderlerToolStripMenuItem.Text = "Giderler";
+            // 
+            // fiyatlandırmaİşlemleriToolStripMenuItem
+            // 
+            this.fiyatlandırmaİşlemleriToolStripMenuItem.Name = "fiyatlandırmaİşlemleriToolStripMenuItem";
+            this.fiyatlandırmaİşlemleriToolStripMenuItem.Size = new System.Drawing.Size(138, 20);
+            this.fiyatlandırmaİşlemleriToolStripMenuItem.Text = "Fiyatlandırma İşlemleri";
+            // 
+            // baskıİşleriToolStripMenuItem
+            // 
+            this.baskıİşleriToolStripMenuItem.Name = "baskıİşleriToolStripMenuItem";
+            this.baskıİşleriToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.baskıİşleriToolStripMenuItem.Text = "Baskı İşleri";
+            // 
+            // kasaİşlemleriToolStripMenuItem
+            // 
+            this.kasaİşlemleriToolStripMenuItem.Name = "kasaİşlemleriToolStripMenuItem";
+            this.kasaİşlemleriToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.kasaİşlemleriToolStripMenuItem.Text = "Kasa işlemleri";
+            // 
+            // dosyaİşlemleriToolStripMenuItem
+            // 
+            this.dosyaİşlemleriToolStripMenuItem.Name = "dosyaİşlemleriToolStripMenuItem";
+            this.dosyaİşlemleriToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.dosyaİşlemleriToolStripMenuItem.Text = "Dosya İşlemleri";
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 72);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(707, 202);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(289, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Randevu Kabul";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Web üzerinden gelen işler ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 261);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.btnSil);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.btnKayit);
+            this.ClientSize = new System.Drawing.Size(735, 330);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Text = "Menü";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnKayit;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button btnSil;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem personelİşlemleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personelKayıtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personelGörüntüleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem müşteriİşlemleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem işOluşturToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yapılacakİşlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem giderlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fiyatlandırmaİşlemleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem baskıİşleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kasaİşlemleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dosyaİşlemleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
