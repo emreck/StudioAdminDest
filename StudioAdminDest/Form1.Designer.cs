bc4b198b93115efbea9192b572f5feea0f888f72
mandalbatmaz21 <nakipali1@gmail.com>
2017-12-09 03:42:10 +0300

    test commiti

--- StudioAdminDest/Form1.Designer.cs
+++ StudioAdminDest/Form1.Designer.cs
@@ -28,18 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnKayit = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnKayit
             // 
-            this.button1.Location = new System.Drawing.Point(168, 193);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Yeni Buton";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnKayit.Location = new System.Drawing.Point(168, 193);
+            this.btnKayit.Name = "btnKayit";
+            this.btnKayit.Size = new System.Drawing.Size(75, 23);
+            this.btnKayit.TabIndex = 0;
+            this.btnKayit.Text = "Yeni Buton";
+            this.btnKayit.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -49,14 +50,25 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(168, 70);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "button1";
+            this.btnSil.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(427, 261);
+            this.Controls.Add(this.btnSil);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnKayit);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -65,8 +77,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnKayit;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSil;
     }
 }
 
--- StudioAdminDest/Form1.cs
+++ StudioAdminDest/Form1.cs
@@ -16,5 +16,10 @@ namespace StudioAdminDest
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            btnSil.Enabled=false
+        }
     }
 }
