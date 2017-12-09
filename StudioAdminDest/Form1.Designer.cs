3a86a5a675fa90371e39485123750392c6a40794
figo_9991 <figo_9991@hotmail.com>
2017-12-09 03:50:18 +0300

    buton adi degisti

--- StudioAdminDest/Form1.Designer.cs
+++ StudioAdminDest/Form1.Designer.cs
@@ -46,11 +46,11 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(271, 193);
+            this.button2.Location = new System.Drawing.Point(302, 225);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "03 50 proje";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
