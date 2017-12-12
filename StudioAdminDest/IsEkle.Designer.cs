caa249f52d0c4a4ff06e7a14849c2b083b92f59d
mandalbatmaz21 <nakipali1@gmail.com>
2017-12-12 08:37:03 +0300

    isekle anatasarim tamam

--- StudioAdminDest/IsEkle.Designer.cs
+++ StudioAdminDest/IsEkle.Designer.cs
@@ -28,53 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.isiEkle = new System.Windows.Forms.Button();
+            this.isAdSoyad = new System.Windows.Forms.TextBox();
+            this.isTel = new System.Windows.Forms.TextBox();
+            this.isMail = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.saatLabel = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.adSoyadlbl = new System.Windows.Forms.Label();
+            this.Telefon = new System.Windows.Forms.Label();
+            this.maillbl = new System.Windows.Forms.Label();
+            this.tarihLabel = new System.Windows.Forms.Label();
+            this.saatLbl = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.referansLbl = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // isiEkle
             // 
-            this.isiEkle.Location = new System.Drawing.Point(202, 232);
+            this.isiEkle.Location = new System.Drawing.Point(151, 213);
             this.isiEkle.Name = "isiEkle";
-            this.isiEkle.Size = new System.Drawing.Size(75, 62);
+            this.isiEkle.Size = new System.Drawing.Size(100, 62);
             this.isiEkle.TabIndex = 0;
             this.isiEkle.Text = "İŞ EKLE";
             this.isiEkle.UseVisualStyleBackColor = true;
+            this.isiEkle.Click += new System.EventHandler(this.isiEkle_Click);
+            // 
+            // isAdSoyad
+            // 
+            this.isAdSoyad.Location = new System.Drawing.Point(154, 24);
+            this.isAdSoyad.Name = "isAdSoyad";
+            this.isAdSoyad.Size = new System.Drawing.Size(141, 20);
+            this.isAdSoyad.TabIndex = 1;
+            // 
+            // isTel
+            // 
+            this.isTel.Location = new System.Drawing.Point(154, 51);
+            this.isTel.Name = "isTel";
+            this.isTel.Size = new System.Drawing.Size(141, 20);
+            this.isTel.TabIndex = 2;
+            // 
+            // isMail
+            // 
+            this.isMail.Location = new System.Drawing.Point(154, 77);
+            this.isMail.Name = "isMail";
+            this.isMail.Size = new System.Drawing.Size(141, 20);
+            this.isMail.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(154, 104);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(141, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17"});
+            this.comboBox1.Location = new System.Drawing.Point(154, 131);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(41, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // saatLabel
+            // 
+            this.saatLabel.AutoSize = true;
+            this.saatLabel.Location = new System.Drawing.Point(201, 133);
+            this.saatLabel.Name = "saatLabel";
+            this.saatLabel.Size = new System.Drawing.Size(10, 13);
+            this.saatLabel.TabIndex = 6;
+            this.saatLabel.Text = ":";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "00",
+            "10",
+            "20",
+            "30",
+            "40",
+            "50"});
+            this.comboBox2.Location = new System.Drawing.Point(217, 130);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(37, 21);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // adSoyadlbl
+            // 
+            this.adSoyadlbl.AutoSize = true;
+            this.adSoyadlbl.Location = new System.Drawing.Point(44, 31);
+            this.adSoyadlbl.Name = "adSoyadlbl";
+            this.adSoyadlbl.Size = new System.Drawing.Size(62, 13);
+            this.adSoyadlbl.TabIndex = 8;
+            this.adSoyadlbl.Text = "AD SOYAD";
+            // 
+            // Telefon
+            // 
+            this.Telefon.AutoSize = true;
+            this.Telefon.Location = new System.Drawing.Point(44, 58);
+            this.Telefon.Name = "Telefon";
+            this.Telefon.Size = new System.Drawing.Size(56, 13);
+            this.Telefon.TabIndex = 9;
+            this.Telefon.Text = "TELEFON";
+            // 
+            // maillbl
+            // 
+            this.maillbl.AutoSize = true;
+            this.maillbl.Location = new System.Drawing.Point(44, 84);
+            this.maillbl.Name = "maillbl";
+            this.maillbl.Size = new System.Drawing.Size(32, 13);
+            this.maillbl.TabIndex = 10;
+            this.maillbl.Text = "MAİL";
+            // 
+            // tarihLabel
+            // 
+            this.tarihLabel.AutoSize = true;
+            this.tarihLabel.Location = new System.Drawing.Point(41, 110);
+            this.tarihLabel.Name = "tarihLabel";
+            this.tarihLabel.Size = new System.Drawing.Size(85, 13);
+            this.tarihLabel.TabIndex = 11;
+            this.tarihLabel.Text = " TESLİM TARİH";
+            // 
+            // saatLbl
+            // 
+            this.saatLbl.AutoSize = true;
+            this.saatLbl.Location = new System.Drawing.Point(44, 133);
+            this.saatLbl.Name = "saatLbl";
+            this.saatLbl.Size = new System.Drawing.Size(35, 13);
+            this.saatLbl.TabIndex = 12;
+            this.saatLbl.Text = "SAAT";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(202, 37);
+            this.textBox1.Location = new System.Drawing.Point(151, 178);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.TabIndex = 13;
             // 
-            // textBox2
+            // referansLbl
             // 
-            this.textBox2.Location = new System.Drawing.Point(202, 64);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 2;
+            this.referansLbl.AutoSize = true;
+            this.referansLbl.Location = new System.Drawing.Point(41, 181);
+            this.referansLbl.Name = "referansLbl";
+            this.referansLbl.Size = new System.Drawing.Size(65, 13);
+            this.referansLbl.TabIndex = 14;
+            this.referansLbl.Text = "REFERANS";
             // 
-            // textBox3
+            // errorProvider1
             // 
-            this.textBox3.Location = new System.Drawing.Point(202, 102);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 3;
+            this.errorProvider1.ContainerControl = this;
             // 
             // IsEkle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 332);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(372, 302);
+            this.Controls.Add(this.referansLbl);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.saatLbl);
+            this.Controls.Add(this.tarihLabel);
+            this.Controls.Add(this.maillbl);
+            this.Controls.Add(this.Telefon);
+            this.Controls.Add(this.adSoyadlbl);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.saatLabel);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.isMail);
+            this.Controls.Add(this.isTel);
+            this.Controls.Add(this.isAdSoyad);
             this.Controls.Add(this.isiEkle);
             this.Name = "IsEkle";
             this.Text = "İŞ EKLE";
+            this.Load += new System.EventHandler(this.IsEkle_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,8 +225,20 @@
         #endregion
 
         private System.Windows.Forms.Button isiEkle;
+        private System.Windows.Forms.TextBox isAdSoyad;
+        private System.Windows.Forms.TextBox isTel;
+        private System.Windows.Forms.TextBox isMail;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label saatLabel;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label adSoyadlbl;
+        private System.Windows.Forms.Label Telefon;
+        private System.Windows.Forms.Label maillbl;
+        private System.Windows.Forms.Label tarihLabel;
+        private System.Windows.Forms.Label saatLbl;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label referansLbl;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- StudioAdminDest/IsEkle.cs
+++ StudioAdminDest/IsEkle.cs
@@ -16,5 +16,53 @@ namespace StudioAdminDest
         {
             InitializeComponent();
         }
+        public bool alanKontrol()
+        {
+            if (string.IsNullOrWhiteSpace(isAdSoyad.Text)|| string.IsNullOrEmpty(isAdSoyad.Text))
+            {
+                errorProvider1.SetError(isAdSoyad, "Lütfen adı soyadı giriniz");
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(isTel.Text) || string.IsNullOrEmpty(isTel.Text))
+            {
+                errorProvider1.SetError(isTel, "Lütfen telefon no giriniz");
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(isMail.Text) || string.IsNullOrEmpty(isMail.Text))
+            {
+                errorProvider1.SetError(isMail, "Lütfen Mail Adresini Giriniz");
+                return false;
+            }
+            return true;
+        }
+        private void IsEkle_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.MinDate = DateTime.Now.AddDays(+1);
+            dateTimePicker1.CustomFormat = "d/M/yyyy";
+        }
+
+        private void isiEkle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (alanKontrol()==false)
+                {
+                    MessageBox.Show("Lütfen degerleri bos bırakmadan giriniz");
+                }
+                else
+                {
+                    MessageBox.Show("BAŞARILI");
+                }
+            }
+            catch(NullReferenceException exp)
+            {
+                MessageBox.Show("HATA : Saat ve diğer değerleri girdiğinize emin olun ");
+            }
+            catch
+            {
+                MessageBox.Show("Doğru değerler girdiğinize emin olun");
+            }
+        }
     }
 }
