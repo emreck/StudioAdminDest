7d20cc2042ea1666a33700d1d4bd766611fc01a6
figo_9991 <figo_9991@hotmail.com>
2017-12-24 04:33:26 +0300

    Dosyalama işlemleri tamam

--- StudioAdminDest/dosyalama.Designer.cs
+++ StudioAdminDest/dosyalama.Designer.cs
@@ -28,32 +28,374 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.isAktar = new System.Windows.Forms.Button();
+            this.isView = new System.Windows.Forms.ListView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader12 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader13 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader14 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader15 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.baskiView = new System.Windows.Forms.ListView();
             this.button1 = new System.Windows.Forms.Button();
+            this.columnHeader16 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.personelad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader17 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader18 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader19 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader20 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader21 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader22 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader23 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.GiderView = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.columnHeader24 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader25 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader26 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader27 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader28 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(2, -2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(859, 345);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.isAktar);
+            this.tabPage1.Controls.Add(this.isView);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(851, 319);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "İşleri Aktar";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // isAktar
+            // 
+            this.isAktar.Location = new System.Drawing.Point(343, 265);
+            this.isAktar.Name = "isAktar";
+            this.isAktar.Size = new System.Drawing.Size(113, 34);
+            this.isAktar.TabIndex = 1;
+            this.isAktar.Text = "Excel\'e Aktar";
+            this.isAktar.UseVisualStyleBackColor = true;
+            this.isAktar.Click += new System.EventHandler(this.isAktar_Click);
+            // 
+            // isView
+            // 
+            this.isView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10,
+            this.columnHeader11,
+            this.columnHeader12,
+            this.columnHeader13,
+            this.columnHeader14,
+            this.columnHeader15});
+            this.isView.Location = new System.Drawing.Point(3, 6);
+            this.isView.Name = "isView";
+            this.isView.Size = new System.Drawing.Size(845, 229);
+            this.isView.TabIndex = 0;
+            this.isView.UseCompatibleStateImageBehavior = false;
+            this.isView.View = System.Windows.Forms.View.Details;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.baskiView);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(851, 319);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Baskı işlerini aktar";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button2);
+            this.tabPage3.Controls.Add(this.GiderView);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(851, 319);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Giderleri Aktar";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            this.tabPage3.Click += new System.EventHandler(this.tabPage3_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Ad Soyad";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tel No";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "İş türü";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Cekim Türü";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Ekstralar";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Çekim Yeri";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Referans";
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "İşi yapanlar";
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Fiyat";
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Tarih";
+            // 
+            // columnHeader11
+            // 
+            this.columnHeader11.Text = "Saat";
+            // 
+            // columnHeader12
+            // 
+            this.columnHeader12.Text = "Teslim tarihi";
+            // 
+            // columnHeader13
+            // 
+            this.columnHeader13.Text = "Ucret Durumu";
+            // 
+            // columnHeader14
+            // 
+            this.columnHeader14.Text = "Not";
+            // 
+            // columnHeader15
+            // 
+            this.columnHeader15.Text = "Teslim Durumu";
+            // 
+            // baskiView
+            // 
+            this.baskiView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader16,
+            this.personelad,
+            this.columnHeader17,
+            this.columnHeader18,
+            this.columnHeader19,
+            this.columnHeader20,
+            this.columnHeader21,
+            this.columnHeader22,
+            this.columnHeader23});
+            this.baskiView.Location = new System.Drawing.Point(7, 7);
+            this.baskiView.Name = "baskiView";
+            this.baskiView.Size = new System.Drawing.Size(841, 223);
+            this.baskiView.TabIndex = 0;
+            this.baskiView.UseCompatibleStateImageBehavior = false;
+            this.baskiView.View = System.Windows.Forms.View.Details;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(36, 56);
+            this.button1.Location = new System.Drawing.Point(334, 249);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Excel\'e Aktar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // columnHeader16
+            // 
+            this.columnHeader16.Text = "Müşteri Ad Soyad";
+            this.columnHeader16.Width = 99;
+            // 
+            // personelad
+            // 
+            this.personelad.Text = "Personel Adı Soyadı";
+            this.personelad.Width = 112;
+            // 
+            // columnHeader17
+            // 
+            this.columnHeader17.Text = "Karton Çerçeve";
+            this.columnHeader17.Width = 101;
+            // 
+            // columnHeader18
+            // 
+            this.columnHeader18.Text = "Plastik Çerçeve";
+            this.columnHeader18.Width = 92;
+            // 
+            // columnHeader19
+            // 
+            this.columnHeader19.Text = "Lüks Çerçeve";
+            this.columnHeader19.Width = 80;
+            // 
+            // columnHeader20
+            // 
+            this.columnHeader20.Text = "Ucretsiz Çerçeve";
+            this.columnHeader20.Width = 93;
+            // 
+            // columnHeader21
+            // 
+            this.columnHeader21.Text = "Toplam Baskı";
+            this.columnHeader21.Width = 85;
+            // 
+            // columnHeader22
+            // 
+            this.columnHeader22.Text = "Ücret";
+            // 
+            // columnHeader23
+            // 
+            this.columnHeader23.Text = "Tarih";
+            this.columnHeader23.Width = 106;
+            // 
+            // GiderView
+            // 
+            this.GiderView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader24,
+            this.columnHeader25,
+            this.columnHeader26,
+            this.columnHeader27,
+            this.columnHeader28});
+            this.GiderView.Location = new System.Drawing.Point(3, 3);
+            this.GiderView.Name = "GiderView";
+            this.GiderView.Size = new System.Drawing.Size(845, 216);
+            this.GiderView.TabIndex = 0;
+            this.GiderView.UseCompatibleStateImageBehavior = false;
+            this.GiderView.View = System.Windows.Forms.View.Details;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(358, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Excel\'e Aktar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // columnHeader24
+            // 
+            this.columnHeader24.Text = "Gider Tipi";
+            this.columnHeader24.Width = 94;
+            // 
+            // columnHeader25
+            // 
+            this.columnHeader25.Text = "Gider Adı";
+            this.columnHeader25.Width = 98;
+            // 
+            // columnHeader26
+            // 
+            this.columnHeader26.Text = "Gider Tutar";
+            this.columnHeader26.Width = 121;
+            // 
+            // columnHeader27
+            // 
+            this.columnHeader27.Text = "Harcamayı Yapan";
+            this.columnHeader27.Width = 104;
+            // 
+            // columnHeader28
+            // 
+            this.columnHeader28.Text = "Tarih";
+            this.columnHeader28.Width = 71;
             // 
             // dosyalama
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(471, 297);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(862, 344);
+            this.Controls.Add(this.tabControl1);
             this.Name = "dosyalama";
             this.Text = "dosyalama";
+            this.Load += new System.EventHandler(this.dosyalama_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button isAktar;
+        private System.Windows.Forms.ListView isView;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.ColumnHeader columnHeader11;
+        private System.Windows.Forms.ColumnHeader columnHeader12;
+        private System.Windows.Forms.ColumnHeader columnHeader13;
+        private System.Windows.Forms.ColumnHeader columnHeader14;
+        private System.Windows.Forms.ColumnHeader columnHeader15;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView baskiView;
+        private System.Windows.Forms.ColumnHeader columnHeader16;
+        private System.Windows.Forms.ColumnHeader personelad;
+        private System.Windows.Forms.ColumnHeader columnHeader17;
+        private System.Windows.Forms.ColumnHeader columnHeader18;
+        private System.Windows.Forms.ColumnHeader columnHeader19;
+        private System.Windows.Forms.ColumnHeader columnHeader20;
+        private System.Windows.Forms.ColumnHeader columnHeader21;
+        private System.Windows.Forms.ColumnHeader columnHeader22;
+        private System.Windows.Forms.ColumnHeader columnHeader23;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView GiderView;
+        private System.Windows.Forms.ColumnHeader columnHeader24;
+        private System.Windows.Forms.ColumnHeader columnHeader25;
+        private System.Windows.Forms.ColumnHeader columnHeader26;
+        private System.Windows.Forms.ColumnHeader columnHeader27;
+        private System.Windows.Forms.ColumnHeader columnHeader28;
     }
 }
--- StudioAdminDest/dosyalama.cs
+++ StudioAdminDest/dosyalama.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using excel = Microsoft.Office.Interop.Excel;
 
 namespace StudioAdminDest
 {
@@ -15,6 +17,216 @@ namespace StudioAdminDest
         public dosyalama()
         {
             InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void isturugosterimi()
+        {
+            isView.Items.Clear(); //verilerin tekrarını gösterme !
+            MySqlConnection baglanti;
+            SqlBaglanti baglan = new SqlBaglanti();
+            baglanti = baglan.baglanti();
+
+            MySqlCommand komut = new MySqlCommand("Select * from Isler where AjansID='" + Form1.ajansID.ToString() + "'", baglanti);
+            MySqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["AdSoyad"].ToString();
+                ekle.SubItems.Add(oku["TelNo"].ToString());
+                ekle.SubItems.Add(oku["IsTuru"].ToString());
+                ekle.SubItems.Add(oku["CekimTuru"].ToString());
+                ekle.SubItems.Add(oku["Ekstralar"].ToString());
+                ekle.SubItems.Add(oku["CekimYeri"].ToString());
+                ekle.SubItems.Add(oku["Referans"].ToString());
+                ekle.SubItems.Add(oku["IsiYapanlar"].ToString());
+                ekle.SubItems.Add(oku["Fiyat"].ToString());
+                ekle.SubItems.Add(oku["Tarih"].ToString());
+                ekle.SubItems.Add(oku["Saat"].ToString());
+                ekle.SubItems.Add(oku["TeslimTarihi"].ToString());
+                ekle.SubItems.Add(oku["UcretDurumu"].ToString());
+                ekle.SubItems.Add(oku["Notlar"].ToString());
+                ekle.SubItems.Add(oku["teslimDurumu"].ToString());
+
+                isView.Items.Add(ekle);
+
+
+                // buraya kadar veritabanından okuduğu yerleri listview'de sırayla yazdı.
+            }
+            baglanti.Close();
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            isView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            isView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+        }
+        public void baskiGoster()
+        {
+            baskiView.Items.Clear(); //verilerin tekrarını gösterme !
+            MySqlConnection baglanti;
+            SqlBaglanti baglan = new SqlBaglanti();
+            baglanti = baglan.baglanti();
+
+            MySqlCommand komut = new MySqlCommand("Select MusAdSoy,PerAdSoy,KartonCer,PlastikCer,LuksCer,UcretsizCer,ToplamBaski,Ucret,Tarih from Baski where AjansID='" + Form1.ajansID.ToString() + "'", baglanti);
+            MySqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["MusAdSoy"].ToString();
+                ekle.SubItems.Add(oku["PerAdSoy"].ToString());
+                ekle.SubItems.Add(oku["KartonCer"].ToString());
+                ekle.SubItems.Add(oku["PlastikCer"].ToString());
+                ekle.SubItems.Add(oku["LuksCer"].ToString());
+                ekle.SubItems.Add(oku["UcretsizCer"].ToString());
+                ekle.SubItems.Add(oku["ToplamBaski"].ToString());
+                ekle.SubItems.Add(oku["Ucret"].ToString());
+                ekle.SubItems.Add(oku["Tarih"].ToString());
+
+                baskiView.Items.Add(ekle);
+
+
+                // buraya kadar veritabanından okuduğu yerleri listview'de sırayla yazdı.
+            }
+            baglanti.Close();
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            baskiView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            baskiView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+        }
+        private void dosyalama_Load(object sender, EventArgs e)
+        {
+
+            isturugosterimi();
+            baskiGoster();
+            giderGoster();
+        }
+
+        private void isAktar_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application xla = new Microsoft.Office.Interop.Excel.Application();
+            xla.Visible = true;
+            Microsoft.Office.Interop.Excel.Workbook wb = xla.Workbooks.Add(Microsoft.Office.Interop.Excel.XlSheetType.xlWorksheet);
+
+            Microsoft.Office.Interop.Excel.Worksheet ws = (Microsoft.Office.Interop.Excel.Worksheet)xla.ActiveSheet;
+            for (int k = 0; k < isView.Columns.Count; k++)
+            {
+                // alanları manuel olarak yazıyoruz
+                ws.Cells[1, k + 1] = isView.Columns[k].Text.ToString();
+            }
+
+            int i = 3;
+            int j = 1;
+            foreach (ListViewItem item in isView.Items)
+            {
+                //ws.Cells[i, j] = item.Text.ToString();
+                foreach (ListViewItem.ListViewSubItem subitem in item.SubItems)
+                {
+                    ws.Cells[i, j] = subitem.Text.ToString();
+                    j++;
+                }
+                j = 1;
+                i++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application xla = new Microsoft.Office.Interop.Excel.Application();
+            xla.Visible = true;
+            Microsoft.Office.Interop.Excel.Workbook wb = xla.Workbooks.Add(Microsoft.Office.Interop.Excel.XlSheetType.xlWorksheet);
+
+            Microsoft.Office.Interop.Excel.Worksheet ws = (Microsoft.Office.Interop.Excel.Worksheet)xla.ActiveSheet;
+            for (int k = 0; k < baskiView.Columns.Count; k++)
+            {
+                // alanları manuel olarak yazıyoruz
+                ws.Cells[1, k + 1] = baskiView.Columns[k].Text.ToString();
+            }
+
+            int i = 3;
+            int j = 1;
+            foreach (ListViewItem item in baskiView.Items)
+            {
+                //ws.Cells[i, j] = item.Text.ToString();
+                foreach (ListViewItem.ListViewSubItem subitem in item.SubItems)
+                {
+                    ws.Cells[i, j] = subitem.Text.ToString();
+                    j++;
+                }
+                j = 1;
+                i++;
+            }
+        }
+        public void giderGoster()
+        {
+            GiderView.Items.Clear(); //verilerin tekrarını gösterme !
+            MySqlConnection baglanti;
+            SqlBaglanti baglan = new SqlBaglanti();
+            baglanti = baglan.baglanti();
+
+            MySqlCommand komut = new MySqlCommand("Select GiderTipi,GiderAdi,HarcamayiYapan,GiderTutar,Tarih,AjansID from Gider where AjansID='" + Form1.ajansID.ToString() + "'", baglanti);
+            MySqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["GiderTipi"].ToString();
+                ekle.SubItems.Add(oku["GiderAdi"].ToString());
+                ekle.SubItems.Add(oku["HarcamayiYapan"].ToString());
+                ekle.SubItems.Add(oku["GiderTutar"].ToString());
+                ekle.SubItems.Add(oku["Tarih"].ToString());
+
+                GiderView.Items.Add(ekle);
+
+
+                // buraya kadar veritabanından okuduğu yerleri listview'de sırayla yazdı.
+            }
+            baglanti.Close();
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            GiderView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            GiderView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application xla = new Microsoft.Office.Interop.Excel.Application();
+            xla.Visible = true;
+            Microsoft.Office.Interop.Excel.Workbook wb = xla.Workbooks.Add(Microsoft.Office.Interop.Excel.XlSheetType.xlWorksheet);
+
+            Microsoft.Office.Interop.Excel.Worksheet ws = (Microsoft.Office.Interop.Excel.Worksheet)xla.ActiveSheet;
+            for (int k = 0; k < GiderView.Columns.Count; k++)
+            {
+                // alanları manuel olarak yazıyoruz
+                ws.Cells[1, k + 1] = GiderView.Columns[k].Text.ToString();
+            }
+
+            int i = 3;
+            int j = 1;
+            foreach (ListViewItem item in GiderView.Items)
+            {
+                //ws.Cells[i, j] = item.Text.ToString();
+                foreach (ListViewItem.ListViewSubItem subitem in item.SubItems)
+                {
+                    ws.Cells[i, j] = subitem.Text.ToString();
+                    j++;
+                }
+                j = 1;
+                i++;
+            }
+
         }
     }
 }
